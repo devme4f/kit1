a72419f7ed4fa336e483739e470dd02e52027dff
devme4f <71699412+devme4f@users.noreply.github.com>
2021-05-06 21:52:15 +0700

    Add files via upload

---
+++ bai1.c
@@ -0,0 +1,14 @@
+//ax + b = 0
+#include<stdio.h>
+int main(){
+	int a, b;
+	printf("Nhap a va b: ");
+	scanf("%d%d", &a, &b);
+	if(a==0 && b!=0){
+		printf("Phuong trinh vo nghiem!");
+	}
+	if(a!=0){
+		printf("X = %.03f\n", -(float)b/a);
+	}
+	return 0;
+}
